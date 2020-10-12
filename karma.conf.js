59bb45296377b2782980112c472cd329b4022a9f
Alex Vasquez <alexvasquez@Alexs-MacBook-Pro.local>
2020-10-12 16:34:56 -0700

    headless v11

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
         base: 'ChromeHeadless',
         flags: [
           '--no-sandbox',
-          '--headless'
+          '--disable-web-security'
         ]
       }
     },
